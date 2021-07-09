06594efd5133c7227402b0464d211fd54c5cdb12
Andy25 <kolokolov71@gmail.com>
2021-07-09 14:30:10 +0300

    упростил 4ю задачу

--- task4.py
+++ task4.py
@@ -1,8 +1,4 @@
 # задача 4
 str = input("введите целое положительное число : ")
-maximum = int(str[0])
-for x in range(1,len(str)):
-    current_number = int(str[x:x+1])
-    if  current_number > maximum :
-        maximum = current_number
+maximum = max(list(str))
 print(f"максимальная цифра равна : {maximum}")
