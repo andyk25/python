937950cf33169e2fb609ef74be8334fa1ac205aa
Andy25 <kolokolov71@gmail.com>
2021-07-09 14:12:19 +0300

    небольшие изменения в коде

--- task3.py
+++ task3.py
@@ -1,3 +1,3 @@
 # задача 3
 num = (input("введите число "))
-print("сумма чисел n + nn + nnn = ", int(num) +int(num + num) + int(num + num + num))
+print("сумма чисел n + nn + nnn = ", int(num) +int(num + num) + int(num * 3))
--- task4.py
+++ task4.py
@@ -2,6 +2,7 @@
 str = input("введите целое положительное число : ")
 maximum = int(str[0])
 for x in range(1,len(str)):
-    if int(str[x:x+1]) > maximum :
-        maximum = int(str[x:x+1])
-print (f"максимальная цифра равна : {maximum}")
+    current_number = int(str[x:x+1])
+    if  current_number > maximum :
+        maximum = current_number
+print(f"максимальная цифра равна : {maximum}")
